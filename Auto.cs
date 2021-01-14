936c2ac59505877f44806fe8cbc3379287a1116a
GuidoVDV <55236307+GuidoVDV@users.noreply.github.com>
2021-01-14 09:32:28 +0100

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toets_210114
+{
+    class Auto
+    {
+        /* Vul deze class aan volgens onderstaande gegevens:
+         * 1. Maak volgende properties (read-write):
+         *      - string Merk
+         *      - string Model
+         *      - int Prijs
+         *      - float Verbruik
+         *  2. Maak volgende property (read-only):
+         *      - float KostPerKM (= 1,25 euro * Verbruik)
+         *  3. Maak volgende methods:
+         *      - AsText: geeft een string terug die de volgende structuur heeft.
+         *          De haakjes verwijzen telkens naar bovenstaande properties.
+         *          De newline's worden gestuurd door het karakter '\n' in de string op te nemen.
+         *      
+         *          Merk: (Merk)    Model: (Model)
+         *          Prijs: €(Prijs) Verbruik: (Verbruik) l/km
+         *          KostPerKM: €(KostPerKM)/km
+         *  4. Maak volgende constructors:
+         *      - default constructor die niets doet
+         *      - constructor met twee argumenten: Merk en Model
+         *      - constructor met vier argumenten: Merk, Model, Prijs, Verbruik
+         */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toets_210114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var auto1 = new Auto();
+            var auto2 = new Auto();
+                auto2.Merk = "Citroën";
+                auto2.Model = "Cactus C4";
+                auto2.Prijs = 27999;
+                auto2.Verbruik = 4.3;
+            var auto3 = new Auto("Peugeot", "5008");
+                auto3.Prijs = 32599;
+                auto3.Verbruik = 5.2;
+            var auto4 = new Auto("Seat", "Ibiza", "14223", "3.8");
+
+            Console.WriteLine(auto1.AsText);
+            Console.WriteLine("");
+            Console.WriteLine(auto2.AsText);
+            Console.WriteLine("");
+            Console.WriteLine(auto3.AsText);
+            Console.WriteLine("");
+            Console.WriteLine(auto4.AsText);
+            Console.ReadKey();
+        }
+    }
+}
